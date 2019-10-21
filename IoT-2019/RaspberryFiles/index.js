ef40d576a253aea5e6f7aa5822d5835b61bcef0f
Lucas Turci <lucas.turci@gmail.com>
2019-10-21 08:56:46 -0300

    Atualizando com o outro git

--- IoT-2019/RaspberryFiles/index.js
+++ IoT-2019/RaspberryFiles/index.js
@@ -1,10 +1,14 @@
 console.log('Hello World');
 
+const PositionCalculator = require('./positionCalculator');
+
+let positionCalculator = new PositionCalculator(0, 0, -5, 6, 4, 6);
+
 const express = require('express');
 const bodyParser = require('body-parser');
 let app = express();
 
-const port = 8081; // when using this port, run node index.js with sudo
+const port = 8081;
 
 var server = app.listen(port, function() {
 	console.log('Listening on ' + port);
@@ -27,9 +31,60 @@ app.get('/send-data', function(req, res) {
 	res.send(pos);
 });
 
+
+n = 2.3;
+d = Math.pow(10, (-67 - (-67))/(10*n));
+console.log(d);
+n = 2.3;
+d = Math.pow(10, (-67 - (-87))/(10*n));
+console.log(d);
+macs = ["CC:50:E3:95:93:64", "24:6F:28:16:6E:08", "3C:71:BF:FB:E2:BC"];
+let measures = []
 app.post('/send-data', function(req, res) {
 	console.log('Received POST request');
 	console.log(req.body);
-	res.send('OK');
+
+	let rssi;
+	if(req.body.rssi != undefined) {
+		rssi = req.body.rssi;
+	} else {
+		res.send('Error: rssi parameter is not set');
+		return;
+	}
+
+	if(req.body.sniffedMac != undefined) {
+		mac = req.body.sniffedMac;
+	} else {
+		res.send('Error: sniffedMac parameter is not set');
+		return;
+	}
+
+	// Formula original eh RSSI = -(10n log_10(d) + A)
+	// Isolando d, d = 10^{(A - RSSI)/10n}
+	// A eh a medicao do RSSI a um metro de distancia
+	// n eh chamado de "environmental factor"
+	// Pra n, eu acho que a gente pega varias medicoes de (RSSI - A)/(10 * log_10(d)) e faz uma media
+	// Entao vamos assumir por enquanto que a gente sabe o A e o n
+	// medicao do RSSI a um metro de distancia
+	A = 67;
+	// medicao do fator de environment
+	n = 2.0;
+
+	let ni = (rssi - A)/(10 * Math.log10(2.65))
+	//console.log("ni = " + ni);
+
+	d = Math.pow(10, (-A - rssi)/(10*n));
+	console.log(d);
+	
+	mac = req.body.sourceMac;
+
+	measures[mac] = d;
+	if(Object.keys(measures).length == 3) {
+		console.log("Resposta");
+		console.log(positionCalculator.calculate(measures[0], measures[1], measures[2]));
+	}
+	
+	let response = `OK, the distance for the sniffed mac ${req.body.sniffedMac} is ${d})`;
+	res.send(response);
 });
 
---
+++ IoT-2019/RaspberryFiles/positionCalculator.js
@@ -0,0 +1,53 @@
+
+// this object is supposed to calculate the position of the beacon
+function positionCalculator(x1, y1, x2, y2, x3, y3) {
+	this.x1 = x1;
+	this.x2 = x2;
+	this.x3 = x3;
+	this.y1 = y1;
+	this.y2 = y2;
+	this.y3 = y3;
+
+	let D = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); // distancia de (x1, y1) pra (x2, y2)
+	
+	// Vamos usar P1 = (x1, y1) e P2 = (x2, y2) pra calcular os dois pontos de interseccao, e entao d3 pra desempatar
+	// d1 = distancia1 ate o mac da esp 2: CC:50:E3:95:93:64
+	// d2 = distancia2 ate o mac da esp 3: 24:6F:28:16:6E:08
+	// d3 = distancia3 ate o mac da esp 4: 3C:71:BF:FB:E2:BC
+	this.calculate = function(d1, d2, d3) {
+		let a = (d1 * d1 - d2 * d2 + D * D)/(2 * D); // distancia do P1 ate o centro da interseccao dos circulos, como explicado em https://stackoverflow.com/questions/3349125/circle-circle-intersection-points 
+		let h = Math.sqrt(d1 * d1 - a * a); // altura ate um ponto de interseccao
+		
+		xmid = this.x1 + a * (this.x2 - this.x1)/D // x do meio
+		ymid = this.y1 + a * (this.y2 - this.y1)/D // y do meio
+		
+		console.log("Centro");
+		console.log([xmid, ymid]);
+
+		let x = xmid + h * (this.y2 - this.y1)/D; // primeiro candidato
+		let y = ymid - h * (this.x2 - this.x1)/D;
+		let xx = xmid - h * (this.y2 - this.y1)/D; // segundo candidato
+		let yy = ymid + h * (this.x2 - this.x1)/D;
+		console.log([x, y])
+		console.log([xx, yy])
+		let d = Math.sqrt((x - this.x3) * (x - this.x3) + (y - this.y3) * (y - this.y3)); // distancia do primeiro candidato ate o terceiro ponto, da esp 4
+		let dd = Math.sqrt((xx - this.x3) * (xx - this.x3) + (yy - this.y3) * (yy - this.y3)); // distancia do segundo candidato ate o terceiro ponto, da esp 4
+
+		// ve qual distancia se aproxima mais da distancia 3, pra retornar
+		if(Math.abs(d - d3) < Math.abs(dd - d3)) {
+			return [x, y];
+		} else {
+			return [xx, yy];
+		}
+	}
+}
+
+
+// Testes
+
+pos = new positionCalculator(0, 0, -5, 6, 4, 6);
+
+console.log(pos.calculate(5, 3, 8.2));
+console.log(pos.calculate(6, 5, 4))
+
+module.exports = positionCalculator;
