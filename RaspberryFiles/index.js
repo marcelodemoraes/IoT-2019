f803982edb10a9667b7ce632be4b9cd4c8b72dc0
Lucas Turci <lucas.turci@gmail.com>
2019-10-21 09:38:01 -0300

    Refatora codigo, implementa resposta de get para /get-data, retornando posicao dos nodes, raios dos circulos e posicao dos beacons
    - Comenta codigos de teste
    - Remove resposta pra get em /send-data

--- RaspberryFiles/index.js
+++ RaspberryFiles/index.js
@@ -2,7 +2,16 @@ console.log('Hello World');
 
 const PositionCalculator = require('./positionCalculator');
 
-let positionCalculator = new PositionCalculator(0, 0, -5, 6, 4, 6);
+const nodes = [
+	{'x': 0, 'y': 0, 'mac': 'CC:50:E3:95:93:64'},
+	{'x': -5, 'y': 6, 'mac': '24:6F:28:16:6E:08'},
+	{'x': 4, 'y': 6, 'mac': '3C:71:BF:FB:E2:BC'}
+];
+
+
+let positionCalculator = 
+	new PositionCalculator(nodes[0].x, nodes[0].y, nodes[1].x, nodes[1].y, nodes[2].x, nodes[2].y);
+
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -23,23 +32,20 @@ app.use(function(req, res, next) {
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
-app.get('/get-data', function(req, res) {
-	console.log('Received GET request');
-	var pos = [];
-	pos.push(Math.floor(Math.random() * 10) - 1);
-	pos.push(Math.floor(Math.random() * 10) - 1);
-	res.send(pos);
-});
 
 
+/*
+Testes da distancia
 n = 2.3;
 d = Math.pow(10, (-67 - (-67))/(10*n));
 console.log(d);
 n = 2.3;
 d = Math.pow(10, (-67 - (-87))/(10*n));
 console.log(d);
-macs = ["CC:50:E3:95:93:64", "24:6F:28:16:6E:08", "3C:71:BF:FB:E2:BC"];
+*/
+
 let measures = []
+let lastMeasures = []
 app.post('/send-data', function(req, res) {
 	console.log('Received POST request');
 	console.log(req.body);
@@ -78,10 +84,41 @@ app.post('/send-data', function(req, res) {
 	measures[mac] = d;
 	if(Object.keys(measures).length == 3) {
 		console.log("Resposta");
-		console.log(positionCalculator.calculate(measures[0], measures[1], measures[2]));
+		console.log(positionCalculator.calculate(measures[nodes[0].mac], measures[nodes[1].mac], measures[nodes[2].mac]));
+		lastMeasures = measures;
+		measures = [];
 	}
 	
 	let response = `OK, the distance for the sniffed mac ${req.body.sniffedMac} is ${d})`;
 	res.send(response);
 });
 
+app.get('/get-data', function(req, res) {
+	console.log('Received GET request');
+
+	let pos = positionCalculator.calculate(lastMeasures[nodes[0].mac], lastMeasures[nodes[1].mac], lastMeasures[nodes[2].mac]);
+	var response = {
+		"beacon": {'x': pos[0], 'y': pos[1]},
+		"sensores": [
+			{
+				"x": nodes[0].x,
+				"y": nodes[0].y,
+				"mac": nodes[0].mac,
+				"raio": lastMeasures[nodes[0].mac],
+			},
+			{
+				"x": nodes[1].x,
+				"y": nodes[1].y,
+				"mac": nodes[1].mac,
+				"raio": lastMeasures[nodes[1].mac],
+			},
+			{
+				"x": nodes[2].x,
+				"y": nodes[2].y,
+				"mac": nodes[2].mac,
+				"raio": lastMeasures[nodes[2].mac],
+			}
+		]
+	};
+	res.send(JSON.stringify(response));
+});
