26fb5a31eee2ef18090ddb9a0b6ff400fe11a5ca
Lucas Turci <lucas.turci@gmail.com>
2019-12-11 15:08:08 -0300

    Consertei front.js

--- Client/front.js
+++ Client/front.js
@@ -3,9 +3,9 @@ var context;
 
 // Coordenadas dos circulos verdes no canvas que indicam a posicao do beacon
 const positions = [
+  [123, 80], // superior esquerda
   [490, 80], // inferior esquerda
   [490, 350], // inferior direita
-  [123, 80], // superior esquerda
   [123, 350] // superior direita
 ];
 const radius = 15; // raio do circulo verde
@@ -15,18 +15,19 @@ const radius = 15; // raio do circulo verde
 function requestData() {
   const url = "http://192.168.1.100:8081/get-data";
   $.get(url, gotData);
+  setTimeout(5000, requestData);
 }
 
 function gotData(data) {
   data = JSON.parse(data);
 
+  clearCanvas();
   Object.keys(data).forEach((mac, i) => {
     let room = data[mac];
     console.log(`Beacon with mac ${mac} is in room ${room}`);
-    //drawCircle(positions[room - 1][0], positions[room - 1][1], radius);
+    drawCircle(positions[room - 1][0], positions[room - 1][1], radius);
   });
 
-  setTimeout(5000, requestData);
 }
 
 // ------------------ RENDERIZACAO ------------------- //
@@ -46,12 +47,10 @@ function drawCircle(x, y, r) {
 // --------------------------------------------------- //
 
 $(function() {
-  console.log("Starting live tracking");
+    console.log('Starting live tracking');
 
-  window.setInterval(function() {
     canvas = document.querySelector("#canvas");
-    context = canvas.getContext("2d");
+    context = canvas.getContext('2d');
     requestData();
-    clearCanvas();
-  }, 3000);
 });
+
