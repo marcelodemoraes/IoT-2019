d5778cfdd3cdd5de98b03dfe8db1a59a1a0e83b2
Lucas Turci <lucas.turci@gmail.com>
2019-12-11 14:55:50 -0300

    Add comentario

--- Client/front.js
+++ Client/front.js
@@ -1,31 +1,20 @@
 var canvas;
 var context;
 
+// Coordenadas dos circulos verdes no canvas que indicam a posicao do beacon
 const positions = [
-  [490, 80],
-  [490, 350],
-  [123, 80],
-  [123, 350]
+  [490, 80], // inferior esquerda
+  [490, 350], // inferior direita
+  [123, 80], // superior esquerda
+  [123, 350] // superior direita
 ];
-const radius = 15;
+const radius = 15; // raio do circulo verde
 
+// Realiza um GET request para o servidor (Raspberry PI), que retorna dicionario JSON com chave MAC e valor: id da sala.
+// Por exemplo {"23:34:B7:23:12": 1, "45:FF:G3:A2:12" : 2}
 function requestData() {
   const url = "http://192.168.1.100:8081/get-data";
   $.get(url, gotData);
-}
-
-function clearCanvas() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function drawCircle(x, y, r) {
-  context.beginPath();
-  context.arc(x, y, r, 0, 2 * Math.PI, false);
-  context.fillStyle = "green";
-  context.fill();
-  context.lineWidth = 1;
-  context.strokeStyle = "#003300";
-  context.stroke();
 }
 
 function gotData(data) {
@@ -39,6 +28,22 @@ function gotData(data) {
 
   setTimeout(5000, requestData);
 }
+
+// ------------------ RENDERIZACAO ------------------- //
+function clearCanvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawCircle(x, y, r) {
+  context.beginPath();
+  context.arc(x, y, r, 0, 2 * Math.PI, false);
+  context.fillStyle = "green";
+  context.fill();
+  context.lineWidth = 1;
+  context.strokeStyle = "#003300";
+  context.stroke();
+}
+// --------------------------------------------------- //
 
 $(function() {
   console.log("Starting live tracking");
